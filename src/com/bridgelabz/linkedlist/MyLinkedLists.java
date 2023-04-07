60654194fb975b80b44fc3d8e1cc15321add66e1
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-07 19:19:08 +0530

    Add: Ability to create Linked List by adding 30 and 56 to 70

--- src/com/bridgelabz/linkedlist/MyLinkedLists.java
+++ src/com/bridgelabz/linkedlist/MyLinkedLists.java
@@ -1,15 +1,41 @@
 package com.bridgelabz.linkedlist;
 
-import java.util.LinkedList;
+public class MyLinkedLists<K> {
+	private MyNode<K> head;
+	private MyNode<K> tail;
 
-public class MyLinkedLists {
+	public void add(K key) {
+		// key value is stored in newNode
+		MyNode<K> newNode = new MyNode<K>(key);
+
+		// if head is null then newNode value is stored head and tail
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} // if head is not null then newNode of next value is stored in head
+		else {
+			newNode.setNext(head);
+			head = newNode;
+		}
+	}
+
+	public void print() {
+		// the value of head is stored in currentNode
+		MyNode<K> currentNode = head;
+		// the condition will go until currentNode is null
+		while (currentNode != null) {
+			System.out.print(currentNode.getKey() + "->");
+			currentNode = currentNode.getNext();
+		}
+		System.out.println("null");
+	}
 
 	public static void main(String[] args) {
-		
-		LinkedList list = new LinkedList();
-		list.add(56);
-		list.add(30);
+
+		MyLinkedLists<Integer> list = new MyLinkedLists<Integer>();
 		list.add(70);
-		System.out.println(list);
+		list.add(30);
+		list.add(56);
+		list.print();
 	}
 }
