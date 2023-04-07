a74c81b94a2fd60d488493a4f2afd140f7d2cc5a
Amrrish1209 <roshanbabubl@gmail.com>
2023-04-07 19:13:38 +0530

    Add: Lets create a simple Linked List of 56, 30 and 70

---
+++ src/com/bridgelabz/linkedlist/MyLinkedLists.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.LinkedList;
+
+public class MyLinkedLists {
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		System.out.println(list);
+	}
+}
